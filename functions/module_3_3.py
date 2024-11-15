f893680d57cee3aef024305a7479fbe8fa584b6a
Дмитрий Назаров <dima_naz2002@mail.ru>
2024-11-15 16:35:12 +0300

    Распаковка позиционных параметров

---
+++ functions/module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a,b,c):
+    print(a,b,c)
+
+# print_params(1,"не строка")
+# print_params(b = 25)
+# print_params(c = [1,2,3])
+
+
+# values_list = [1,"string", False]
+values_dict = {'a': 1, 'b': "строка", 'c': True}
+print_params(**values_dict)
+
+# values_list_2 = [4, "vol"]
+# print_params(*values_list_2, 42)
